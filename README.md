376dd8b042a974c32b258710fc2a251b7a9fc426
Sangsan <sangsan.prohmvitak@gmail.com>
2025-02-22 06:41:02 +0700

    Add: dashboard that can run concurrently in app.py
    1. can dev run in dashboard.py
    2. can run multi-threading in app.py
    Need to revise and design what kind of charts to add on Dasboard.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # learn-dashb-a-db
 Run PostgreSQL in a docker container and query it into a dashboard with python.
+
+Dashboard design
+- top paying customers
+- [ ] top selling products
+- [ ] what else?
--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-# app.py
 from dao import FetchDAO
 from database import SessionLocal
+from dashboard import app
+import threading
 
 
 def main():
@@ -12,5 +13,18 @@ def main():
         session.close()
 
 
+def run_dash():
+    # Start the Dash app in a separate thread
+    app.run_server(debug=True, use_reloader=False)  # `use_reloader=False` to avoid running twice
+
+
 if __name__ == "__main__":
+    # Run both the Dash app and FetchDAO logic concurrently
+    # dash_thread = threading.Thread(target=run_dash)
+    # dash_thread.start()
+
+    # Run the original main logic (FetchDAO)
     main()
+
+    # Ensure the Dash app keeps running in the background
+    # dash_thread.join()
